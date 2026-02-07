4eacc432c78d77edf179671cdfc9566d161391ac
Dom Samphier <dominicsamphier@gmail.com>
2026-02-07 17:33:33 +0000

    feat: add a preview path for contentful

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require 'dotenv/load'
 
-set :host_authorization, permitted_hosts: ['.fly.dev', '.domsamphier.com']
+set :host_authorization, permitted_hosts: ['.fly.dev', '.domsamphier.com', '0.0.0.0']
+
 require 'contentful'
 require 'json'
 require 'rich_text_renderer'
@@ -26,5 +27,25 @@ get '/posts/:id' do
     { title: entry.fields[:title], content: content }.to_json
   else
     error 406
+  end
+end
+
+# Contentful allows previewing entries in an iframe on their site.
+# To enable this, we need to adjust the security headers accordingly.
+# Also I'm validating the preview token passed in the URL.
+#
+get '/posts/preview/:id' do
+  headers 'X-Frame-Options' => '',
+          'Content-Security-Policy' => "frame-ancestors 'self' https://app.contentful.com"
+
+  url_preview_token = params['preview_token']
+  if url_preview_token.to_s.empty?
+    halt 401, 'Unauthorized: Missing preview token'
+  elsif url_preview_token != ENV['CONTENTFUL_PREVIEW_ACCESS_TOKEN']
+    halt 403, 'Forbidden: Invalid preview token'
+  else
+    entry = client.entry(params[:id])
+    content = rich_text_renderer.render(entry.fields[:content])
+    erb :post, locals: { title: entry.fields[:title], content: content }
   end
 end
